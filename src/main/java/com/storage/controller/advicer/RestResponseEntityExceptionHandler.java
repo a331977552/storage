4bf6b38fc96735e4aab2f08679db0838f4eb44ab
cody huang <a13521874221@gmail.com>
2018-11-25 09:30:06 +0000

    added infinite scroll .

--- src/main/java/com/storage/controller/advicer/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/storage/controller/advicer/RestResponseEntityExceptionHandler.java
@@ -17,15 +17,15 @@ public class RestResponseEntityExceptionHandler {
     	String message = ex.getMessage();
     	String bodyOfResponse;
     	if(message.startsWith("The temporary upload location")) {
-    		bodyOfResponse = "请联系管理员";  
+    		bodyOfResponse = "服务器异常! code: 99";
     	}else
     	if(message.contains("Load balancer does not")) {
-    		bodyOfResponse="back service has gone";
+    		bodyOfResponse="服务器异常! code: 100";
     	}else {
-    		bodyOfResponse=message+"code:"+ 109;
+    		bodyOfResponse=message+"code: 109";
     	}
     	if(ex instanceof feign.RetryableException) {
-    		bodyOfResponse="服务器异常! code:"+ 110;
+    		bodyOfResponse="服务器异常! code: 110";
     	}
     	ModelAndView andView=new ModelAndView();
     	andView.addObject("error",bodyOfResponse);
